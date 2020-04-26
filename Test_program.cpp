891df693a9b03980ec0f8aac0f14e4d748427065
Wuttisun Pratpayon <49830121+Texas-rangers@users.noreply.github.com>
2020-04-26 14:36:52 +0700

    Create Test_program.cpp
    temporarily code

---
+++ Test_program.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    ios_base::sync_with_stdio(0);
+    int N; cin >> N;
+    for(int i=1;i <+ N;i++){
+        for(int l=1;l <= i;l++){
+            cout << "Hello ";
+         }
+         cout << "\n";
+     }
+     return 0;
+}
